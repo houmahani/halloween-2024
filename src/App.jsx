4306214c5d2481ed0c4d0e28ae38f753b83dc3d2
Houmahani Kane <houmahanikane@gmail.com>
2024-10-07 22:39:45 +0200

    🧑‍💻 Adds leva debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment } from '@react-three/drei'
+import { Leva, useControls } from 'leva'
 import Experience from './Experience/Experience.jsx'
 
 import './App.css'
 
 function App() {
+  const { canvasColor } = useControls('Canvas Background', {
+    canvasColor: '#233f5b',
+  })
+
+  const { fogColor } = useControls('Fog Color', {
+    fogColor: '#233f5b',
+  })
+
   return (
-    <Canvas>
-      <color attach="background" args={['#0b0429']} />
-      <Environment preset="dawn" />
-      <Experience />
-    </Canvas>
+    <>
+      <Leva />
+      <Canvas>
+        <color attach="background" args={[canvasColor]} />
+        <fog attach="fog" args={[fogColor, 0, 20]} />
+        <Environment preset="dawn" />
+        <Experience />
+      </Canvas>
+    </>
   )
 }
 
--- src/Experience/background/Background.jsx
+++ src/Experience/background/Background.jsx
@@ -1,6 +1,11 @@
 import { MeshReflectorMaterial } from '@react-three/drei'
+import { useControls } from 'leva'
 
 export default function Background() {
+  const { color } = useControls('floorColor', {
+    color: '#0d1421',
+  })
+
   return (
     <>
       <mesh position={[0, -3, 0]} rotation={[-Math.PI / 2, 0, 0]}>
@@ -12,7 +17,7 @@ export default function Background() {
           mixStrength={15}
           depthScale={1}
           minDepthThreshold={0.85}
-          color="#0b0429"
+          color={color}
           metalness={0.6}
           roughness={1}
         />
